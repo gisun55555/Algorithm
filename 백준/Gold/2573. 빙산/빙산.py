7e10c2e4bc59a2371216eb55cd31e176f1ac6f81
gisun55555 <139519062+gisun55555@users.noreply.github.com>
2023-12-22 20:32:04 +0900

    [Gold IV] Title: 빙산, Time: 2904 ms, Memory: 216452 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,20 +1,23 @@
 from collections import deque
 import copy
 
+
+# 빙산 녹이는 함수
 def melt(y,x): #빙산녹는다
     ct=0 #차감할 함수
     for i in range(4):
         dy = y + dyl[i]
         dx = x + dxl[i]
-        if arr3[dy][dx] == 0:
+        if arr3[dy][dx] == 0:   #복사본 기준으로 확인해주는게 중요하다..!
             ct +=1
     
-    if arr[y][x] - ct < 0:
+    if arr[y][x] - ct < 0: # 0보다 작을 경우 0으로 배출
         arr[y][x] =0
         return
-    else:
+    else: # 아닐 경우 기존 수식 진행
         arr[y][x] = arr[y][x] - ct
 
+# 하나의 섬을 카운터하는 bfs
 def bfs(y,x):
     q = deque()
     q.append([y,x])
@@ -40,14 +43,15 @@ arr= [ list(map(int,input().split())) for i in range(N)]
 
 # print(arr)
 
-rs=0
+rs=0 #몇 년후에 되는지 카운터하는 변수
 
-flag=0
+flag=0 # arr 배열에서 모두 0 이 아니라면 발동하는 플래그
 
 while flag ==0:
     rs+=1 #결과값 출력
-    flag =1
-    arr3 = copy.deepcopy(arr) # 빙하 녹이는 비교용!
+    flag =1 
+    arr3 = copy.deepcopy(arr) # 빙하 녹이는 비교용! 중요 이게 없으면 arr녹으면서 확인하는 거라 비교를 위한 복사본 필요하다..!
+
     for i in range(N):
         for j in range(M):
             if arr[i][j] != 0:
