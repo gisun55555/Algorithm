183a8180232efa32494a6f475cf97190565dbbe6
오기선 <139519062+gisun55555@users.noreply.github.com>
2024-08-03 22:46:11 +0900

    [Silver IV] Title: 최대 GCD, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9417. 최대 GCD/최대 GCD.py
@@ -0,0 +1,17 @@
+n = int(input())
+numList = [list(map(int,input().split())) for _ in range(n)]
+
+# 최대 GCD
+def gcd(a,b):
+    while b>0:
+        a,b = b, a%b
+    return a
+
+
+for list in numList:
+    ansList=[]
+    for i in range(len(list)):
+        for j in range(i,len(list)):
+            if i!=j:
+                ansList.append(gcd(list[i],list[j]))
+    print(max(ansList))
