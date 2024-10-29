c7ffecd5f1a16371bee5122035b0c83882862c7b
오기선 <139519062+gisun55555@users.noreply.github.com>
2024-10-29 19:32:36 +0900

    [level 0] Title: rny_string, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.js
+++ 프로그래머스/0/181863. rny＿string/rny＿string.js
@@ -1,17 +1,3 @@
 function solution(rny_string) {
-    let result = ''
-    
-    for(let i=0; i<rny_string.length; i++){
-        if(rny_string[i]==='m'){
-            result+= 'rn'
-            
-        }else{
-            result+=rny_string[i]
-        }
-        
-    }
-    
-    
-    
-    return result;
+    return rny_string.replaceAll('m', "rn");
 }
