1936bea7fc67e8f934b56eb0427cb48ae201f9fd
오기선 <139519062+gisun55555@users.noreply.github.com>
2024-11-01 16:02:46 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 245.28 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -0,0 +1,16 @@
+def solution(s):
+    def expand_around_center(left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1 
+
+    max_length = 1
+
+    for i in range(len(s)):
+        len1 = expand_around_center(i, i)
+        len2 = expand_around_center(i, i + 1)
+        
+        max_length = max(max_length, len1, len2)
+
+    return max_length
