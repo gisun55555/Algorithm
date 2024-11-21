b3fcf2c6a16a3929272b3764f6a92966351a288f
오기선 <139519062+gisun55555@users.noreply.github.com>
2024-11-21 15:46:59 +0900

    [level 3] Title: 여행경로, Time: 336.45 ms, Memory: 14.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,30 +1,34 @@
 def solution(tickets):
+    
     tickets.sort()
-    result = []  
-    target = [""] * (len(tickets) + 1)  
-    used = [0] * len(tickets)  
-
-    def dfs(end, num):
-        if num == len(tickets):  
-            result.append(target[:])  
+    used = [0] * len(tickets)
+    channel = [0] * (len(tickets)+1)
+    result = []
+    
+    def dfs(end,num):
+        channel[num]=end
+        
+        if num == len(tickets):
+            result.append(channel[:])
+            
             return
-
+        
         for i in range(len(tickets)):
-            if used[i] == 1: 
-                continue
-            if tickets[i][0] == end:  
-                used[i] = 1 
-                target[num + 1] = tickets[i][1]  
-                dfs(tickets[i][1], num + 1)  
-                target[num + 1] = "" 
-                used[i] = 0  
-
+            if used[i]==1: continue
+            if tickets[i][0]==end: 
+                used[i]=1
+                dfs(tickets[i][1],num+1)
+                used[i]=0
+                channel[num+1]=0
+                
     for i in range(len(tickets)):
-        if tickets[i][0] == 'ICN':  
-            used[i] = 1  
-            target[0] = 'ICN'  
-            target[1] = tickets[i][1]  
-            dfs(tickets[i][1], 1)  
-            used[i] = 0 
-
+        if tickets[i][0] == "ICN":
+            used[i]=1
+            channel[0]=tickets[i][0]
+            dfs(tickets[i][1],1)
+            channel[0]=0
+            used[i]=0
+        
+        
+    # print(result)
     return result[0]
