442198a3f8279b57498681de0fc475053519fa91
오기선 <139519062+gisun55555@users.noreply.github.com>
2024-11-28 13:29:46 +0900

    [level 3] Title: 여행경로, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -2,29 +2,27 @@ from collections import defaultdict
 
 
 def solution(tickets):
-    
     graph = defaultdict(list)
-    for a, b in tickets:
+    
+    for a,b in tickets:
         graph[a].append(b)
-        # print(graph)
+    print(graph)
     
     for key in graph:
         graph[key].sort(reverse=True)
-        # print(graph)
+    route =[]
     
-    route = []
-
     def dfs(airport):
         while graph[airport]:
-            print(graph)
             next_airport = graph[airport].pop()
             dfs(next_airport)
         route.append(airport)
-        print(route)
-
+    
     dfs("ICN")
+    
+        
+    
+    
     return route[::-1]
 
 
-print(solution([["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]))
-print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]))
