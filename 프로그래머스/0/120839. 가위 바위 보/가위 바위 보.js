23169676368e0c480474f965689a3d62b3c6d4b7
오기선 <139519062+gisun55555@users.noreply.github.com>
2024-11-30 15:15:34 +0900

    [level 0] Title: 가위 바위 보, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,16 +1,9 @@
 function solution(rsp) {
-    var answer = [];
     
+    const winMap ={'2':'0','0':'5','5':'2'}
     
-    for(i=0; i<rsp.length; i++){
-        if(rsp[i]==='2'){
-            answer.push(0)
-        } else if(rsp[i]==='0'){
-            answer.push(5)
-        } else if(rsp[i]==='5'){
-        answer.push(2)}
-    }
-        
+    let answer=[...rsp].map( choice=>winMap[choice] )
+
     return answer.join('')
         
         
