367fd2225c2d10caf05f2bf3ede108bfa7b1b49f
오기선 <139519062+gisun55555@users.noreply.github.com>
2024-12-24 19:41:05 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -0,0 +1,9 @@
+function solution(a, b) {
+    const concat =Number(String(a)+String(b))
+    
+    const productValue = 2*a*b
+    
+    return Math.max(concat,productValue)
+    
+    return answer;
+}
