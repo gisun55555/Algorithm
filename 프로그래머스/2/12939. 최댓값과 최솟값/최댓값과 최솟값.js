e4fbec45bb174e4c0ef3f1c7e15949a242e8b73b
오기선 <139519062+gisun55555@users.noreply.github.com>
2024-12-31 19:32:15 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,10 @@
+function solution(s) {
+    const numbers = s.split(' ').map(Number);
+
+    const min = Math.min(...numbers);
+    const max = Math.max(...numbers);
+
+    return `${min} ${max}`;
+}
+
+
