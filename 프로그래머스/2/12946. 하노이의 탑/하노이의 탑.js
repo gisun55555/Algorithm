f20fdc4233966bc6385fce3f0f9e47ac34116cc2
오기선 <139519062+gisun55555@users.noreply.github.com>
2025-01-21 21:49:27 +0900

    [level 2] Title: 하노이의 탑, Time: 10.76 ms, Memory: 48.8 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.js
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.js
@@ -1,21 +1,22 @@
-function solution(n) {
-    const moves = [];
-
-    function hanoi(num, start, support, goal) {
-        if (num === 1) {
-            moves.push([start, goal]);
-            return;
+function solution(x){
+    const move =[]
+    
+    function hanoi(n, start, mid,goal){
+        
+        if(n===1){
+            move.push([start,goal])
+            return
         }
-
-        hanoi(num - 1, start, goal, support);
-
-        moves.push([start, goal]);
-
-        hanoi(num - 1, support, start, goal);
+        
+        hanoi(n-1,start,goal,mid)
+        move.push([start,goal])
+        hanoi(n-1,mid,start,goal)
+        
+        
     }
-
-    hanoi(n, 1, 2, 3);
-
-    return moves;
+    hanoi(x,1,2,3)
+    
+    
+    return move
+    
 }
-
