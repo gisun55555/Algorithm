58f6242326ee2c28e6427ad7167d0d16c59f4543
오기선 <139519062+gisun55555@users.noreply.github.com>
2025-03-01 23:51:18 +0900

    [level 3] Title: 가장 먼 노드, Time: 65.55 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -1,39 +1,46 @@
 function solution(n, vertex) {
     
-    const graph = Array.from({ length:n+1},()=>[])
-    
-    vertex.forEach(([a,b])=>{
-        graph[a].push(b)
-        graph[b].push(a)
+ const array = Array.from({length:n+1},()=>[])
+ const distance = Array(n+1).fill(-1)
+ console.log(distance)
+ 
+ console.log(array)
+ 
+ for(let i=0;i<vertex.length;i++){
+     const [a,b] = vertex[i]
+     array[a].push(b)
+     array[b].push(a)
+     
+ }
+       
+    console.log(array)
+
+    const q=[1]
+    distance[1]=0
+
+    while (q.length >0) {
+        let node = q.shift()
         
-    })
-    
-    const distances = Array(n+1).fill(-1)
-    const queue = [1]
-    distances[1]=0
-    
-    while(queue.length){
-        const node = queue.shift()
-        
-        for(const next of graph[node]){
-            if(distances[next] === -1){
-                
-                distances[next] = distances[node]+1
-                queue.push(next)
-            }
+        for(target of array[node]){
+            if(distance[target] === -1){
+            distance[target]=distance[node]+1
+            q.push(target)}
             
             
         }
         
-        
     }
-    const maxDistance = Math.max(...distances)
     
-    
-    
-    
-    return distances.filter(d => d === maxDistance).length;
+    const max = Math.max(...distance)
+    console.log(distance)
+
+       
+       return distance.filter(num=>num===max).length
 }
 
 
-    
+    // 2차원 배열만들고
+ // 집어넣기
+ // bfs로 후딱 찾기
+ // 노드로 찾고 넣어주기
+ // distance에 그전 값 +1
