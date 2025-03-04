04eae64dcbcd94f125a30eeadadb01d8a59f1a4b
오기선 <139519062+gisun55555@users.noreply.github.com>
2025-03-04 19:08:28 +0900

    [level 3] Title: 순위, Time: 25.73 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.js
+++ 프로그래머스/3/49191. 순위/순위.js
@@ -1,40 +1,43 @@
 function solution(n, results) {
     
     
-    const graph = Array.from({length:n+1},()=>Array(n+1).fill(false))
-    
+    //2차원 배열그리자..!
+    const arr = Array.from({length:n+1},()=>Array.from(n+1).fill(false))
     
     results.forEach(([winner,loser])=>{
-        graph[winner][loser] = true
-        
+        arr[winner][loser]=true
     })
     
     
-    // 플로이드 워셜 중간 경로 있다면 연결
-    for(let k=1; k<=n;k++){
-        for(let i=1; i<=n; i++){
+    //플로이드 로샬쓰고
+    for(let k=1;k<=n;k++){
+        for(let i=1;i<=n;i++){
             for(let j=1; j<=n;j++){
-                if(graph[i][k]&&graph[k][j])
-                graph[i][j] = true
+                if(arr[i][k]&&arr[k][j]){
+                    arr[i][j]=true
+                }
             }
         }
     }
     
     
-    let count =0
+    //나빼고 모든 로드알면 이긴거구나..!
+    
+    let answer=0
     for(let i=1;i<=n;i++){
-        let knownMatches =0
+        let cnt=0
         for(let j=1;j<=n;j++){
-            if(graph[i][j]||graph[j][i]){
-                knownMatches+=1
+            if(arr[i][j]||arr[j][i]){
+                cnt+=1
             }
             
         }
-        if(knownMatches == n-1) count++
-        
+        if (cnt===n-1){
+            answer+=1
+        }
     }
     
     
     
-    return count;
+    return answer;
 }
