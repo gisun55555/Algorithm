b9ddbc2db0117b1f84eddb0d5772e4ab1424ca5e
오기선 <139519062+gisun55555@users.noreply.github.com>
2025-03-10 23:46:07 +0900

    [level 3] Title: 순위, Time: 52.10 ms, Memory: 42.8 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.js
+++ 프로그래머스/3/49191. 순위/순위.js
@@ -1,43 +1,55 @@
 function solution(n, results) {
+    // 2차원 배열만들기
+    // 빵구 칠하기
+    // 플루이드 마셜 진행시키기
+    // 정확하게 순위 매길수 있는 친구들 구하기
+    let answer=[]
     
     
-    //2차원 배열그리자..!
-    const arr = Array.from({length:n+1},()=>Array.from(n+1).fill(false))
+    const arr = Array.from({length:n+1},()=>new Array(n+1).fill(false))
+    console.log(arr)
     
-    results.forEach(([winner,loser])=>{
-        arr[winner][loser]=true
-    })
+    results.forEach(([win,lose])=>arr[win][lose]= true)
+    console.log(arr)
     
-    
-    //플로이드 로샬쓰고
     for(let k=1;k<=n;k++){
         for(let i=1;i<=n;i++){
-            for(let j=1; j<=n;j++){
-                if(arr[i][k]&&arr[k][j]){
+            for(let j=1;j<=n;j++){
+                if(arr[i][k]===true&&arr[k][j]===true){
                     arr[i][j]=true
                 }
+                
             }
         }
     }
     
+    console.log(arr)
     
-    //나빼고 모든 로드알면 이긴거구나..!
-    
-    let answer=0
-    for(let i=1;i<=n;i++){
+    for(let k=1;k<=n;k++){
         let cnt=0
-        for(let j=1;j<=n;j++){
-            if(arr[i][j]||arr[j][i]){
-                cnt+=1
+        for(let i=1;i<=n;i++){
+                if(arr[k][i]===true || arr[i][k]===true){
+                    cnt+=1
+                    if(cnt===n-1){
+                        answer.push(k)
+                    }
+                    
             }
-            
-        }
-        if (cnt===n-1){
-            answer+=1
         }
     }
+    console.log(answer)
     
     
     
-    return answer;
+    
+    
+    
+    
+    
+    //arr[i][k]&&arr[k][j] 이거어색한지 파악
+    
+    
+    
+    
+    return answer.length
 }
