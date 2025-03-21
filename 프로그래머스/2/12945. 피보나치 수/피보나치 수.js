98bff86ecd55bc268eb4c0af7ba0ede174c9a533
오기선 <139519062+gisun55555@users.noreply.github.com>
2025-03-21 23:22:39 +0900

    [level 2] Title: 피보나치 수, Time: 2.72 ms, Memory: 36.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,13 @@
+function solution(n) {
+  const MOD = 1234567;
+  let prev = 0;
+  let curr = 1;
+
+  for (let i = 2; i <= n; i++) {
+    const next = (prev + curr) % MOD;
+    prev = curr;
+    curr = next;
+  }
+
+  return curr;
+}
